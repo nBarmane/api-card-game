f80c380917e5311213353a303096684ff798ed51
Nouamane BARMANE <n.barmane@gmail.com>
2023-02-24 20:45:54 +0100

    init project

---
+++ src/main/java/com/atexotest/apicardgame/ApiCardGameApplication.java
@@ -0,0 +1,13 @@
+package com.atexotest.apicardgame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiCardGameApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiCardGameApplication.class, args);
+	}
+
+}
