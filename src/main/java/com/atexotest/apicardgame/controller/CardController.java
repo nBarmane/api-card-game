122d5f4c480d2926b8eaf0511d5fe5155bf281e1
Nouamane BARMANE <n.barmane@gmail.com>
2023-02-25 01:00:52 +0100

    fix cors

--- src/main/java/com/atexotest/apicardgame/controller/CardController.java
+++ src/main/java/com/atexotest/apicardgame/controller/CardController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/card-game/api")
-@CrossOrigin(origins = "4200")
+@CrossOrigin(origins = "http://localhost:4200")
 public class CardController {
     @Autowired
     private CardService cardService;
